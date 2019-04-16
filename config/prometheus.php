4245a6aa9f23925f7242bc5c9921c1d3769b6997
陈德华 <chendehua@enmonster.org>
2019-04-16 11:42:52 +0800

    * 增加配置文件

---
+++ config/prometheus.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'namespace' => '',
+
+    'conn'  => 'default',
+
+    'name' => 'http_server_requests_seconds',
+
+    'help'  => 'duration of http_requests',
+
+    'prefix'    => 'bes:'.env('APP_ENV').':prometheus',
+
+    'namespace_http_server' => 'http',
+
+    'histogram_buckets' => [1500, 3000],
+];
